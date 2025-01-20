bf0234e6d3425a6ec50965a5cfe9f65187f2b5d0
mfahimaamir <mfahim204s8@gmail.com>
2025-01-20 10:56:29 +0500

    Add files via upload

--- test668.py
+++ test668.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import pyodbc
 import pandas as pd
-conn_str=('DRIVER={Microsoft Access Driver (accdb)};DBQ=mfa.accdb;')
+#conn_str=('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=mfa.accdb;')
+
+conn_str = (
+    r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+    r'DBQ=./mfa.accdb;'
+)
 connm = pyodbc.connect(conn_str)
 cursorm = connm.cursor()
 query = "SELECT * FROM Payments"
 dataf = pd.read_sql(query, connm)
 mm = st.dataframe()
 st.write(dataf)
+
