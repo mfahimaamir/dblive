518ffff5d10160665887ba6c6b9687a29119bf97
mfahimaamir <mfahim204s8@gmail.com>
2025-01-20 09:33:08 +0500

    Add files via upload

--- test668.py
+++ test668.py
@@ -5,7 +5,7 @@ conn_str=('DRIVER={Microsoft Access Driver (*.accdb)};DBQ=mfa.accdb;')
 #conn_str = (
 #    r'DRIVER={Microsoft Access Driver (*.accdb)};'
 #    r'DBQ=mfa.accdb;'
-)
+#)
 connm = pyodbc.connect(conn_str)
 cursorm = connm.cursor()
 query = "SELECT * FROM Payments"
